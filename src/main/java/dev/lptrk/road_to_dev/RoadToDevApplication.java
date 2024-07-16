14266e7492fbbf85632be5445bbe1951bad60ddb
lptrk <lktpriv@gmail.com>
2024-07-16 15:20:42 +0200

    init

---
+++ src/main/java/dev/lptrk/road_to_dev/RoadToDevApplication.java
@@ -0,0 +1,13 @@
+package dev.lptrk.road_to_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadToDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadToDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lptrk/road_to_dev/RoadToDevApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lptrk.road_to_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadToDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
