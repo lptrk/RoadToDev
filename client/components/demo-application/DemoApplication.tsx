06e698f9bb87eb4f0365bdc8ffbc537d8a841a1d
lptrk <lktpriv@gmail.com>
2024-07-17 12:32:23 +0200

    feat(demo-application): Implement Navbar-component

--- client/components/demo-application/DemoApplication.tsx
+++ client/components/demo-application/DemoApplication.tsx
@@ -1,7 +1,13 @@
+import Navbar from "@/components/navbar";
+
 export default function DemoApplication() {
   return (
-    <div className={"border border-black rounded-lg h-[80%] w-[90%]"}>
-      Hallo
+    <div
+      className={
+        "bg-white border border-gray-300 rounded-lg h-[80%] w-[90%] p-4"
+      }
+    >
+      <Navbar />
     </div>
   );
 }
