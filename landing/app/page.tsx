72f99b0b59650d81326585018e521f550dc21122
lptrk <lktpriv@gmail.com>
2024-07-17 13:13:06 +0200

    feat(landing): embed demo-application inside of landing page

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "@/components/navbar";
+import DemoApplication from "@/components/demo-application";
 
 export default function Home() {
   return (
     <div className={"h-full w-full flex items-center justify-center"}>
-      {/*<Navbar />*/}
+      <DemoApplication />
     </div>
   );
 }
